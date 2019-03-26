69f61492a2b1a0dab94dc59214d8efb39f595395
Clem-Fern <clement.coussieu@gmail.com>
2019-03-26 22:13:06 +0100

    Fix auth proccess & add debug message & http server default page

--- socket-server.js
+++ socket-server.js
@@ -1,5 +1,29 @@
-var io = require('socket.io')();
-var jwt = require('jsonwebtoken');
+var http = require('http');
+const jwt = require('jsonwebtoken');
+
+var server = http.createServer(function(req, res) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end("API : ON");
+});
+
+const io = require('socket.io').listen(server);
+
+/*use(function(socket, next){
+	  if (socket.handshake.query && socket.handshake.query.token){
+	    jwt.verify(socket.handshake.query.token, 'SECRET_KEY', function(err, decoded) {
+	      if(err) return next(console.log(new Error('Authentication error')));
+	      socket.decoded = decoded;
+	      next();
+	    });
+	  } else {
+	      next(console.log(new Error('Authentication error')));
+	  }
+	})*/
+	io.sockets.on('connection', function(socket) {
+	    // Connection now authenticated to receive further events
+		console.log("user connected")
 
 
+	});
 
+server.listen(8080);
