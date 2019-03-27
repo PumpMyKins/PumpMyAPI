db846500cf0e60b819bbf6d0e0ee840d10eed02b
Clem-Fern <clement.coussieu@gmail.com>
2019-03-27 22:00:54 +0100

    Add TokensManager & MessagesManager in Manager module

---
+++ Manager.js
@@ -0,0 +1,43 @@
+module.exports = {
+  getTokensManager: function () {
+    return new TokensManager();
+  },
+  getMessagesManager: function (t) {
+    return new MessagesManager(t);
+  }
+};
+
+function TokensManager(){
+  let map = new Map();
+  /////////////////////////////////////
+  // Add token
+
+  map.set('token1','bungee');
+
+  /////////////////////////////////////
+  this.tokens = map;
+}
+
+TokensManager.prototype.verify = function (token,callback) {
+
+  if(!token || !this.tokens.has(token)) return callback(new Error("Invalid Token"));
+
+  return callback(null,this.tokens.get(token));
+};
+
+
+function MessagesManager(man,io){
+
+  this.tokensManager = man;
+  this.io = io;
+  this.messages = new Map();
+
+}
+
+MessagesManager.prototype.addPendingMessage = function (data,callback) {
+
+  if(!data) return callback(new Error("Invalid Token"));
+
+  return callback(null,this.tokens.get(token));
+
+};
