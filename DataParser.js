61d57738ed999295b25cb1a8ad10222542d166ec
Clem-Fern <clement.coussieu@gmail.com>
2019-03-28 13:03:17 +0100

    Update Message class

--- DataParser.js
+++ DataParser.js
@@ -1,15 +1,20 @@
 module.exports = {
-  getTokensManager: function (data) {
-    return new Message(data);
+  getTokensManager: function (sender,data) {
+    return new Message(sender,data);
   },
 };
 
-function Message(data){
+function Message(sender,data){
+  this.sender = sender._id;
+  this.data = data;
   this.parser = JSON.parse(data);
 }
 
+Message.prototype.sender = this.sender;
 Message.prototype.target = this.parser.target;
 Message.prototype.ID = this.parser.messageID;
 Message.prototype.timestamp = this.parser.timestamp;
 
 Message.prototype.content = this.parser.content;
+
+Message.prototype.data = this.data;
